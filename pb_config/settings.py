54144f020b9a79cbb51da0911ae6788b78f09b84
mmac.pers@gmail.com <michael@sixtymill.com>
2017-12-12 23:23:26 -0600

    asthetic changes, and added in static configurations in settings to change for local and prod.

--- pb_config/settings.py
+++ pb_config/settings.py
@@ -184,24 +184,25 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_URL = '/static/'
 
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = 'static'
+if os.getcwd() == '/app':
 
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+    }
+    AWS_LOCATION = 'static'
 
-STATICFILES_DIRS = [
+    STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'bookings/static'),
-]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = os.environ.get("STATICFILES_STORAGE")
+    ]
+    STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    STATICFILES_STORAGE = os.environ.get("STATICFILES_STORAGE")
 
-#STATIC_URL = '/static/'
 
 # Stripe Settings
 # local settings 
