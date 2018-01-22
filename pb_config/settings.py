373547634bab70a17d62871aac09bc90fde80dc5
mmac.pers@gmail.com <michael@sixtymill.com>
2018-01-22 14:55:31 -0600

    Removed SSL settings because build failed while trying to collect static.

--- pb_config/settings.py
+++ pb_config/settings.py
@@ -57,10 +57,10 @@ if os.getcwd() == '/app':
 
     SITE_ID = 1
 
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    #SECURE_SSL_REDIRECT = True
+    #SESSION_COOKIE_SECURE = True
+    #CSRF_COOKIE_SECURE = True
+    #SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 # Application definition
